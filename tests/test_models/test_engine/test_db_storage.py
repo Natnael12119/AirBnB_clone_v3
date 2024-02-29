fba8a6d0068dcb7edddc3f52899f3f1d834d1da7
Natnael12119 <nhattydeveloper@gmail.com>
2024-02-29 01:36:58 -0800

    Test file created

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -27,7 +27,7 @@ class TestDBStorageDocs(unittest.TestCase):
     """Tests to check the documentation and style of DBStorage class"""
     @classmethod
     def setUpClass(cls):
-        """Set up fmeor the doc tests"""
+        """Set up for the doc tests"""
         cls.dbs_f = inspect.getmembers(DBStorage, inspect.isfunction)
 
     def test_pep8_conformance_db_storage(self):
@@ -87,19 +87,29 @@ class TestFileStorage(unittest.TestCase):
     def test_save(self):
         """Test that save properly saves objects to file.json"""
 
-    @unittest.skipIf(models.storage_t != 'db', "not testing file storage")
-    def test_count(self):
-        """ Test for the FS count method
-        """
-        num_states = models.storage.count(State)
-        state = State(name="statey")
-        state.save()
-        self.assertEqual(models.storage.count(State), num_states + 1)
-
-    @unittest.skipIf(models.storage_t != 'db', "not testing file storage")
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
     def test_get(self):
-        """ Test for FS get method
-        """
-        state = State(name="PR")
-        state.save()
-        self.assertIs(models.storage.get(State, state.id), state)
+        """ Tests method for obtaining an instance db storage"""
+        storage = DBStorage
+        storage.reload()
+        dic = {"name": "Cundinamarca"}
+        instance = State(**dic)
+        storage.new(instance)
+        storage.save()
+        get_instance = storage.get(State, instance.id)
+        self.assertEqual(get_instance, instance)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_count(self):
+        """ Tests count method db storage """
+        storage = DBStorage
+        storage.reload()
+        dic = {"name": "Vecindad"}
+        state = State(**dic)
+        storage.new(state)
+        dic = {"name": "Mexico", "state_id": state.id}
+        city = City(**dic)
+        storage.new(city)
+        storage.save()
+        c = storage.count()
+        self.assertEqual(len(storage.all()), c)
